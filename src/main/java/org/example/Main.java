bd3d26a48103afc80e7a927cf37ccbf964f94791
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-06-23 23:16:15 +0200

    Init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.google.common.base.Joiner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String... args) {
+        LOGGER.info("Hello, you");
+        LOGGER.debug("You provided arguments: {}", Joiner.on(", ").join(args));
+        LOGGER.info("End of program");
+    }
+
+}
