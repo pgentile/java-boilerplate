e7ddcc3c4ce4095ed800c92e6da5f641cf415cde
Pierre Gentile <pierre.gentile.perso@gmail.com>
2016-07-28 21:27:21 +0200

    Sample JUnit 5 test, integration with Gradle build

---
+++ src/test/java/org/example/SampleTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SampleTest {
+
+    /**
+     * Sample single test.
+     */
+    @Test
+    public void hello_should_be_nice() {
+        assertThat("Hello!").endsWith("!");
+    }
+
+    /**
+     * Sample test factory.
+     *
+     * @return Dynamic tests
+     */
+    @TestFactory
+    public Stream<DynamicTest> a_non_empty_string_must_have_a_positive_size() {
+        return Stream.of("titi", "toto", "toto")
+            .map(str -> {
+                return DynamicTest.dynamicTest("'" + str + "' must have a positive size", () -> {
+                    assertThat(str.length()).isPositive();
+                });
+            });
+    }
+
+}
